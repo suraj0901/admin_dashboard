cbda9cab56544f0793f9a384974c084d3aa658e2
suraj0901 <66919989+suraj0901@users.noreply.github.com>
2022-09-03 10:50:38 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
 
 app.listen(PORT, () => `listening on  port ${PORT}`);
